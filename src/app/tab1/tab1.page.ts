e46090785d108a05a8ce1cbf49b4cf2d7009600d
deep9699 <deepbhavsar9@gmail.com>
2020-07-25 18:05:59 +0530

    intial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  slider: IonSlides;
+
   constructor() {}
+
+  slidesDidLoad(mySlider: IonSlides)
+  {
+    this.slider = mySlider;
+    mySlider.startAutoplay();
+  }
 
 }
